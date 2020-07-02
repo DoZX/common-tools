d5592892ef52a2a5716c8d6d98cbba7d614dc86f
dozx <dozx1996@hotmail.com>
2020-07-03 01:04:09 +0800

    m: update ct-elasticsearch docker file

---
+++ system-ct-server/elasticsearch/docker-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DOCKERFILE_PATH=./
+IMAGE_NAME=ct-elasticsearch
+IMAGE_TAG=1.0
+
+run() {
+	name=$1
+	sudo docker run -itd -p 9200:9200 -p 9600:9600 --name=${IMAGE_NAME}-${name} ${IMAGE_NAME}:${IMAGE_TAG} /bin/bash
+}
+
+del() {
+	name=$1
+	sudo docker rm -f ${IMAGE_NAME}-${name}
+}
+
+docker_name="test-01"
+del $docker_name
+run $docker_name
+
+# win
+# docker build --network=host --no-cache -t ct-elasticsearch:0.1 ./
+# docker rm -f ct-elasticsearch
+# docker run -itd -p 9200:9200 -p 9600:9600 --name=ct-elasticsearch ct-elasticsearch:0.1 /bin/bash
+# docker push ct-elasticsearch:0.1
+
+# docker logs --tail=100 -f ct-elasticsearch
+# docker exec -it ct-elasticsearch bash
