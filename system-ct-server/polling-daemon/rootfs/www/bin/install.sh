c36ae2ecebe2ca66817ce6bb73b3d19dc2ef27b3
dozx <dozx1996@hotmail.com>
2020-07-23 22:42:31 +0800

    m: update pip.whl

--- system-ct-server/polling-daemon/rootfs/www/bin/install.sh
+++ system-ct-server/polling-daemon/rootfs/www/bin/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PACKAGE_PATH='/www/tengine-server/zip'
+TENGINE_PACKAGE_PATH='/www/bin/zip/tengine_lua'
+PYTHON_PACKAGE_PATH='/www/bin/zip/python'
 LUA_INSTALL_PATH='/www/tengine-server/lua_resources'
 TENGINE_INSTALL_PATH='/www/tengine-server/server'
 
@@ -9,23 +10,23 @@ if [ ! -e $LUA_INSTALL_PATH ]; then
 fi
 
 init_install_packages() {
-    cd $PACKAGE_PATH
-    if [ ! -e $PACKAGE_PATH/tengine-2.2.0.tar.gz ]; then
+    cd $TENGINE_PACKAGE_PATH
+    if [ ! -e $TENGINE_PACKAGE_PATH/tengine-2.2.0.tar.gz ]; then
         wget http://tengine.taobao.org/download/tengine-2.2.0.tar.gz
     fi
-    if [ ! -e $PACKAGE_PATH/ngx_cache_purge-2.3.tar.gz ]; then
+    if [ ! -e $TENGINE_PACKAGE_PATH/ngx_cache_purge-2.3.tar.gz ]; then
         wget http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz
     fi
-    if [ ! -e $PACKAGE_PATH/v0.3.0rc1.tar.gz ]; then
+    if [ ! -e $TENGINE_PACKAGE_PATH/v0.3.0rc1.tar.gz ]; then
         wget https://github.com/simpl/ngx_devel_kit/archive/v0.3.0rc1.tar.gz
     fi
-    if [ ! -e $PACKAGE_PATH/LuaJIT-2.0.4.tar.gz ]; then
+    if [ ! -e $TENGINE_PACKAGE_PATH/LuaJIT-2.0.4.tar.gz ]; then
         wget http://luajit.org/download/LuaJIT-2.0.4.tar.gz
     fi
-    if [ ! -e $PACKAGE_PATH/v0.10.2.tar.gz  ]; then
+    if [ ! -e $TENGINE_PACKAGE_PATH/v0.10.2.tar.gz  ]; then
         wget https://github.com/openresty/lua-nginx-module/archive/v0.10.2.tar.gz
     fi
-    if [ ! -e $PACKAGE_PATH/pcre-8.38.tar.gz  ]; then
+    if [ ! -e $TENGINE_PACKAGE_PATH/pcre-8.38.tar.gz  ]; then
         wget http://downloads.sourceforge.net/project/pcre/pcre/8.38/pcre-8.38.tar.gz
     fi
 
@@ -38,7 +39,7 @@ init_install_packages() {
 }
 
 make_install_lua() {
-    cd $PACKAGE_PATH/LuaJIT-2.0.4 && make && make install PREFIX=$LUA_INSTALL_PATH
+    cd $TENGINE_PACKAGE_PATH/LuaJIT-2.0.4 && make && make install PREFIX=$LUA_INSTALL_PATH
     grep -i -n "^export LUAJIT_LIB=" /etc/profile
     if [ $? -ne 0 ]; then
         echo 'export LUAJIT_LIB='$LUA_INSTALL_PATH'/lib' >> /etc/profile && \
@@ -48,15 +49,15 @@ make_install_lua() {
 }
 
 make_install_tengine() {
-    cd $PACKAGE_PATH/tengine-2.2.0 && \
+    cd $TENGINE_PACKAGE_PATH/tengine-2.2.0 && \
     ./configure --prefix=$TENGINE_INSTALL_PATH \
-                --with-pcre=$PACKAGE_PATH/pcre-8.38 \
+                --with-pcre=$TENGINE_PACKAGE_PATH/pcre-8.38 \
                 --with-debug \
-                --add-module=$PACKAGE_PATH/ngx_cache_purge-2.3 \
+                --add-module=$TENGINE_PACKAGE_PATH/ngx_cache_purge-2.3 \
                 --with-http_stub_status_module \
                 --with-http_ssl_module \
-                --add-module=$PACKAGE_PATH/ngx_devel_kit-0.3.0rc1 \
-                --add-module=$PACKAGE_PATH/lua-nginx-module-0.10.2/ \
+                --add-module=$TENGINE_PACKAGE_PATH/ngx_devel_kit-0.3.0rc1 \
+                --add-module=$TENGINE_PACKAGE_PATH/lua-nginx-module-0.10.2/ \
                 --with-ld-opt=-Wl,-rpath,$LUA_INSTALL_PATH/lib && \
     make && make install
     mv -f /www/tengine-server/conf/nginx.conf $TENGINE_INSTALL_PATH/conf/nginx.conf && \
@@ -64,7 +65,8 @@ make_install_tengine() {
 }
 
 install_python_packages() {
-    /usr/bin/pip install --upgrade pip && /usr/bin/pip install requests configparser pyyaml
+    /usr/bin/pip install --upgrade $PYTHON_PACKAGE_PATH/pip-20.1.1-py2.py3-none-any.whl && \
+    /usr/bin/pip install requests configparser pyyaml
 }
 
 main() {
