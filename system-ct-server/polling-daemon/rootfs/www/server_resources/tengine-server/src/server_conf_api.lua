77d27352ecab5b07cdd9561391dfbbb0ab938e1d
dozx <dozx1996@hotmail.com>
2020-08-18 00:19:51 +0800

    m: append pd-tengine server

--- system-ct-server/polling-daemon/rootfs/www/server_resources/tengine-server/src/server_conf_api.lua
+++ system-ct-server/polling-daemon/rootfs/www/server_resources/tengine-server/src/server_conf_api.lua
@@ -12,6 +12,59 @@ local function do_response(log_level, result)
     ngx.exit(conf.RESPONCE_CODE_SUCCESS)
 end
 
+local function split(str, reps)
+    local resultStrList = {}
+    string.gsub(str,'[^'..reps..']+',function ( w )
+        table.insert(resultStrList, w)
+    end)
+    return resultStrList
+end
+
+local function get_es_conf(es_ip_list, token)
+    local es_conf_table = {}
+    es_cluster_name = "ct-elasticsearch"
+    es_http_port = 9200
+    es_tcp_port = 9300
+    es_minimum_master_nodes = #es_ip_list / 2 + 1
+    es_start_node_name = "ct-node-"
+    es_initial_master_nodes = {}
+    es_seed_hosts = {}
+    for num, es_ip in ipairs(es_ip_list) do
+        es_initial_master_nodes[num] = es_start_node_name .. num
+        es_seed_hosts[num] = es_ip .. ":" .. es_tcp_port
+
+        local es_conf = {}
+        es_conf["cluster_name"] = es_cluster_name
+        es_conf["http_port"] = es_http_port
+        es_conf["tcp_port"] = es_tcp_port
+        es_conf["minimum_master_nodes"] = es_minimum_master_nodes
+        es_conf["node_name"] = es_start_node_name .. num
+        es_conf["initial_master_nodes"] = es_initial_master_nodes
+        es_conf["seed_hosts"] = es_seed_hosts
+        es_conf["token"] = token
+        es_conf_table[es_ip] = es_conf
+    end
+    return es_conf_table
+end
+
+local function get_kibana_conf(kibana_ip_list, es_ip_list, token)
+    local kibana_conf_table = {}
+    kibana_server_name = "ct-kibana"
+    es_http_port = 9200
+    elasticsearch_hosts = {}
+    for num, es_ip in ipairs(es_ip_list) do
+        elasticsearch_hosts = es_ip .. ":" .. es_http_port
+    end
+    for num, kibana_ip in ipairs(kibana_ip_list) do
+        local kibana_conf = {}
+        kibana_conf["server_name"] = kibana_server_name
+        kibana_conf["elasticsearch_hosts"] = elasticsearch_hosts
+        kibana_conf["token"] = token
+        kibana_conf_table[kibana_ip] = kibana_conf
+    end
+    return kibana_conf_table
+end
+
 local function get_conf(key)
     local status = conf.RESPONCE_CODE_SUCCESS
     local msg = "get [" .. key ..  "] conf success"
@@ -29,14 +82,23 @@ local function reload_conf()
     local status = conf.RESPONCE_CODE_SUCCESS
     local msg = "success"
     local iniconf = iniUtil.load(conf.CT_SERVER_CONF)
+    -- token
     local token = iniUtil.get(iniconf, conf.INI_CT_SERVER_CONF_SECTION, conf.INI_TOKEN, "admin:admin")
     shared_conf:set(conf.CT_SERVER_SHARED_CONF_KEY .. conf.INI_TOKEN, '"'.. token .. '"', conf.CHECK_SHARED_TIMEOUT, os.time() + 1)
+    -- es
     local es_node_ips = iniUtil.get(iniconf, conf.INI_CT_SERVER_CONF_SECTION, conf.INI_ES_NODE_IPS, "127.0.0.1")
-    -- ip > cluster.name node.name
+    local es_ip_list = split(es_node_ips, ",")
+    local es_conf_table = get_es_conf(es_ip_list)
+    for es_ip, es_conf in ipairs(es_conf_table, token) do
+        shared_conf:set(conf.CT_SERVER_SHARED_CONF_KEY .. es_ip, dkjson.encode(es_conf), conf.CHECK_SHARED_TIMEOUT, os.time() + 1)
+    end
+    -- kibana
     local kibana_node_ips = iniUtil.get(iniconf, conf.INI_CT_SERVER_CONF_SECTION, conf.INI_KIBANA_NODE_IPS, "127.0.0.1")
-    -- ip > server.name
-    local pd_tengine_node_ip = iniUtil.get(iniconf, conf.INI_CT_SERVER_CONF_SECTION, conf.INI_PD_TENGINE_NODE_IP, "127.0.0.1")
-
+    local kibana_ip_list = split(kibana_node_ips, ",")
+    local kibana_conf_table = get_kibana_conf(kibana_ip_list, es_ip_list, token)
+    for kibana_ip, kibana_conf in ipairs(kibana_conf_table) do
+        shared_conf:set(conf.CT_SERVER_SHARED_CONF_KEY .. kibana_ip, dkjson.encode(kibana_conf), conf.CHECK_SHARED_TIMEOUT, os.time() + 1)
+    end
     local conf_data = dkjson.encode(iniconf)
     local data = '{"status":' .. status .. ', "msg":"' .. msg .. '", "data":'.. conf_data .. '}'
     do_response(conf.LOG_INFO, data)
