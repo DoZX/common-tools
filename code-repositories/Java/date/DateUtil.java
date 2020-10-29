14290d64afc213ecbb2241b5445794af86bfb474
houry <2507511047@qq.com>
2020-10-29 17:07:35 +0800

    优化日期工具类

--- code-repositories/Java/date/DateUtil.java
+++ code-repositories/Java/date/DateUtil.java
@@ -16,15 +16,31 @@ import java.util.stream.Stream;
  * Date: 2020/10/27 10:48
  * Version: V1.0
  */
-public class DateUtil {
-    /**
-     * 日期格式化格式
-     */
-    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
-    /**
-     * 日期格式化格式
-     */
-    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+public class DateUtils {
+
+    public enum DateType {
+        NORM_DATE_PATTERN("yyyy-MM-dd"),
+        NORM_DATETIME_PATTERN("yyyy-MM-dd HH:mm:ss"),
+        NORM_TIME_PATTERN("HH:mm:ss"),
+        NORM_DATETIME_MINUTE_PATTERN("yyyy-MM-dd HH:mm"),
+        NORM_DATETIME_MS_PATTERN("yyyy-MM-dd HH:mm:ss.SSS"),
+        CHINESE_DATE_PATTERN("yyyy年MM月dd日"),
+        PURE_DATE_PATTERN("yyyyMMdd"),
+        PURE_TIME_PATTERN("HHmmss"),
+        PURE_DATETIME_PATTERN("yyyyMMddHHmmss"),
+        PURE_DATETIME_MS_PATTERN("yyyyMMddHHmmssSSS"),
+        PURE_DATE_MD_PATTERN("MMdd"),
+        ;
+        private String format;
+
+        DateType(String format) {
+            this.format = format;
+        }
+
+        public String getFormat() {
+            return format;
+        }
+    }
 
     /**
      * 定义一周的每天表示的常量，用于返回中文展示
@@ -44,42 +60,71 @@ public class DateUtil {
     }
 
     /**
-     * 将字符串转换成时间戳
+     * 把日期转换成指定格式的字符串日期
      *
-     * @param time   字符串格式的时间
-     * @param format 格式化日期格式
-     * @return long类型的时间戳
+     * @param date 传递的日期
+     * @param type 指定格式
+     * @return 字符串日期
      */
-    public static long strToStamp(String time, String format) {
-        if (YYYY_MM_DD.equals(format)) {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
-            LocalDate parse = LocalDate.parse(time, dateTimeFormatter);
-            return LocalDate.from(parse).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        } else if (YYYY_MM_DD_HH_MM_SS.equals(format)) {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
-            LocalDateTime parse = LocalDateTime.parse(time, dateTimeFormatter);
-            return LocalDateTime.from(parse).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        }
-        return 0L;
+    public static String dateToString(Date date, DateType type) {
+        ZonedDateTime now = dateToZonedDateTime(date);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(type.getFormat());
+        return now.format(formatter);
+    }
+
+    /**
+     * 把日期转换成字默认的符串日期
+     *
+     * @param date 传递日期
+     * @return 字符串日期
+     */
+    public static String dateToString(Date date) {
+        return dateToString(date, DateType.NORM_DATETIME_MINUTE_PATTERN);
+    }
+
+
+    /**
+     * 把当前日期格式化成指定的的字符串日期
+     *
+     * @param type 指定的格式化日期
+     * @return 字符串日期
+     */
+    public static String dateToString(DateType type) {
+        return dateToString(new Date(), type);
+    }
+
+    /**
+     * 把当前日期格式化成默认的字符串日期
+     *
+     * @return 字符串日期
+     */
+    public static String dateToString() {
+        return dateToString(new Date(), DateType.NORM_DATETIME_MINUTE_PATTERN);
     }
 
     /**
      * 将字符串日期转换成Date
      *
-     * @param time 时间字符串
-     * @return Date时间
+     * @param dateStr  日期字符串，可以是 20200101、2020-01-01、2020年01月01日等格式
+     * @param dateType 格式化日期类型，必须包含完整的年月日
+     * @return Date类型日期
      */
-    public static Date strToDate(String time, String format) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
-        if (YYYY_MM_DD_HH_MM_SS.equals(format)) {
-            LocalDateTime localDateTime = LocalDateTime.parse(time, dateTimeFormatter);
-            return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-        } else if (YYYY_MM_DD.equals(format)) {
-            LocalDate localDate = LocalDate.parse(time, dateTimeFormatter);
-            return localDateToDate(localDate);
-        } else {
-            return null;
-        }
+    public static Date strToDate(String dateStr, DateType dateType) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateType.getFormat());
+        return Date.from(LocalDate.parse(dateStr, formatter).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    }
+
+    /**
+     * 将字符串日期转换成Date
+     *
+     * @param dateStr  日期字符串，可以是 "20200101131313"或者"2020-01-01 13:13:13"格式
+     * @param dateType 格式化日期类型，必须包含完整的年月日时分秒
+     * @return Date类型日期
+     */
+    public static Date strToDateTime(String dateStr, DateType dateType) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateType.getFormat());
+        return Date.from(LocalDateTime.parse(dateStr, formatter).atZone(ZoneId.systemDefault()).toInstant());
     }
 
     /**
@@ -92,45 +137,6 @@ public class DateUtil {
         return LocalDate.now().equals(dateToLocalDate(date));
     }
 
-    /**
-     * 把Date类型的日志转换成LocalDate类型的日期
-     *
-     * @param date Date类型日期
-     * @return LocalDate类型日期
-     */
-    public static LocalDate dateToLocalDate(Date date) {
-        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-    }
-
-    /**
-     * 把LocalDate类型的日志转换成Date类型的日期
-     *
-     * @param localDate LocalDate类型日期
-     * @return Date类型日期
-     */
-    public static Date localDateToDate(LocalDate localDate) {
-        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-    }
-
-    /**
-     * 把Date类型的日志转换成LocalDateTime类型的日期
-     *
-     * @param date Date类型日期
-     * @return LocalDateTime类型日期
-     */
-    public static LocalDateTime dateToLocalDateTime(Date date) {
-        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-    }
-
-    /**
-     * 把LocalDateTime类型的日志转换成Date类型的日期
-     *
-     * @param localDateTime LocalDateTime类型日期
-     * @return Date类型日期
-     */
-    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
-        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-    }
 
     /**
      * 获取给定日期之前或者之后的日期
@@ -147,7 +153,7 @@ public class DateUtil {
     }
 
     /**
-     * 获取给定日期之前或者之后的月份
+     * 获取当前日期之前或者之后的月份
      * beforeOrAfterNumMonth(0) 当月 2020-10
      * beforeOrAfterNumMonth(-1) 当天的前一月 2020-09
      * beforeOrAfterNumMonth(1) 当天的后一月 2020-11
@@ -161,35 +167,93 @@ public class DateUtil {
     }
 
     /**
-     * 给当前时间增加日期返回增加日期之后的时间
+     * 给当前时间增加年数
      *
-     * @param nowDate 当前时间
-     * @param type    选择增加的类型
-     * @param num     增加的数量
+     * @param date 当前时间
+     * @param num  增加的数量
      * @return 返回增加日期之后的新日期
      */
-    public static Date addDate(Date nowDate, String type, int num) {
-        switch (type) {
-            case "year":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusYears(num));
-            case "month":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusMonths(num));
-            case "day":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusDays(num));
-            case "hours":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusHours(num));
-            case "minutes":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusMinutes(num));
-            case "seconds":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusSeconds(num));
-            case "nanos":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusNanos(num));
-            case "weeks":
-                return localDateTimeToDate(dateToLocalDateTime(nowDate).plusWeeks(num));
-            default:
-                return nowDate;
-        }
+    public static Date addYear(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusYears(num));
     }
+
+    /**
+     * 给当前时间增加月数
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addMonth(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusMonths(num));
+    }
+
+    /**
+     * 给当前时间增加天数
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addDay(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusDays(num));
+    }
+
+    /**
+     * 给当前时间增加小时
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addHours(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusHours(num));
+    }
+
+    /**
+     * 给当前时间增加分钟
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addMinutes(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusMinutes(num));
+    }
+
+    /**
+     * 给当前时间增加秒数
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addSeconds(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusSeconds(num));
+    }
+
+    /**
+     * 给当前时间增加纳秒
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addNanos(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusNanos(num));
+    }
+
+    /**
+     * 给当前时间增加周数
+     *
+     * @param date 当前时间
+     * @param num  增加的数量
+     * @return 返回增加日期之后的新日期
+     */
+    public static Date addWeeks(Date date, int num) {
+        return localDateTimeToDate(dateToLocalDateTime(date).plusWeeks(num));
+    }
+
 
     /**
      * 获取当天是星期几
@@ -378,7 +442,7 @@ public class DateUtil {
      * @param seconds 给定的秒数
      * @return 格式化之后的时间字符串
      */
-    public static String getFormatBySeconds(int seconds) {
+    public static String getTimeBySeconds(int seconds) {
         if (seconds > 60 * 60 * 24 - 1) {
             return "23:59:59";
         }
@@ -415,6 +479,67 @@ public class DateUtil {
         long days = ChronoUnit.DAYS.between(startDate, endDate);
         LongStream.range(0, days + 1).forEach(d -> dayList.add(startDate.plusDays(d).toString()));
         return dayList;
+    }
+
+
+    /**
+     * 把Date类型的日期转化成ZoneDateTime类型的日期
+     *
+     * @param date date类型日期
+     * @return ZoneDateTime类型日期
+     */
+    private static ZonedDateTime dateToZonedDateTime(Date date) {
+        return ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    /**
+     * 把ZoneDateTime类型的日期转化成Date类型的日期
+     *
+     * @param zonedDateTime ZoneDateTime类型日期
+     * @return date类型日期
+     */
+    private static Date zonedDateTimeToDate(ZonedDateTime zonedDateTime) {
+        return Date.from(zonedDateTime.toInstant());
+    }
+
+    /**
+     * 把Date类型的日志转换成LocalDate类型的日期
+     *
+     * @param date Date类型日期
+     * @return LocalDate类型日期
+     */
+    private static LocalDate dateToLocalDate(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    /**
+     * 把LocalDate类型的日志转换成Date类型的日期
+     *
+     * @param localDate LocalDate类型日期
+     * @return Date类型日期
+     */
+    private static Date localDateToDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * 把Date类型的日志转换成LocalDateTime类型的日期
+     *
+     * @param date Date类型日期
+     * @return LocalDateTime类型日期
+     */
+    private static LocalDateTime dateToLocalDateTime(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    /**
+     * 把LocalDateTime类型的日志转换成Date类型的日期
+     *
+     * @param localDateTime LocalDateTime类型日期
+     * @return Date类型日期
+     */
+    private static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
 }
